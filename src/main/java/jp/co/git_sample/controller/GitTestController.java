043493d43566f5711bef4effda78ea8de1788a4c
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-05-20 14:05:44 +0900

    アノテーションを追加

--- src/main/java/jp/co/git_sample/controller/GitTestController.java
+++ src/main/java/jp/co/git_sample/controller/GitTestController.java
@@ -1,5 +1,10 @@
 package jp.co.git_sample.controller;
 
-public class GitTestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/gittest")
+public class GitTestController {
+	
 }
